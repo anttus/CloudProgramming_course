05ae90eac70655838789259dd87e43b2318b1c29
Anttu Suhonen <anttu.suhonen@gmail.com>
2019-01-14 11:16:25 +0200

    Assignment 1

--- w1 JavaScript Language/01 JavaScript Tools/js/guess.js
+++ w1 JavaScript Language/01 JavaScript Tools/js/guess.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let targetNumber = Math.floor(Math.random() * 10) + 1;
 let guessNumber = 0;
 
@@ -10,13 +12,16 @@ function init() {
 }
 
 function check(value) {
-	if (value == targetNumber) showWin();
-	else if (value != targetNumber && guessNumber >= 5) showLoss();
-	else showError();
+	if (!isNaN(value)) {
+		if (value == targetNumber) showWin();
+		else if (value != targetNumber && guessNumber >= 5) showLoss();
+		else showError();
+	} else console.log("Not a number");
 }
 
 function showWin() {
 	console.log("You win.");
+	removeForm();
 }
 
 function showError() {
@@ -25,6 +30,12 @@ function showError() {
 
 function showLoss() {
 	console.log("You lose.");
+	removeForm();
+}
+
+function removeForm() {
+	let form = document.getElementById('inputForm');
+	form.remove();
 }
 
 init();

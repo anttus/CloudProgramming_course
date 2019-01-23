ac693e8626bef4071b7ff9a0bbe87eb986dc7f43
Anttu Suhonen <anttu.suhonen@gmail.com>
2019-01-23 16:47:25 +0200

    Assignment w3_1 done

--- w3 Vue/01 Introduction/js/logic.js
+++ w3 Vue/01 Introduction/js/logic.js
@@ -1,8 +1,7 @@
-STORAGE_KEY = 'results_key';
+let STORAGE_KEY = 'results_key';
 let resultStorage = {
     fetch: function () {
         let results = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
-        console.log(localStorage.getItem(STORAGE_KEY));
         results.forEach((result, index) => {
             result.id = index;
         });
@@ -12,7 +11,7 @@ let resultStorage = {
     save: function (results) {
         localStorage.setItem(STORAGE_KEY, JSON.stringify(results));
     }
-}
+};
 
 function isNotNumericValue(value) {
     return isNaN(value) || !isFinite(value);
@@ -21,35 +20,36 @@ function isNotNumericValue(value) {
 const calc = new Vue({
     el: '#app',
     data: { x: '', y: '', result: '', results: resultStorage.fetch(), },
+    watch: { // !WATCH RIGHT AFTER DATA
+        results: {
+            handler(results) {
+                resultStorage.save(results);
+            },
+            deep: true
+        }
+    },
     methods: {
         multiply: function () {
             let x = parseFloat(this.x);
             if (isNotNumericValue(x)) return this.result;
             let y = parseFloat(this.y);
             if (isNotNumericValue(y)) return this.result;
-
+            
             res = x * y;
             this.result = res;
-
+            this.results.push(res);
         },
         divide: function () {
             let x = parseFloat(this.x);
             if (isNotNumericValue(x)) return this.result;
             let y = parseFloat(this.y);
             if (isNotNumericValue(y)) return this.result;
-
+            
             if (this.y != 0) res = x / y;
             else res = "Division by zero.";
-
+            
             this.result = res;
+            this.results.push(res);
         },
-        // watch: {
-        //     results: {
-        //         handler (results) {
-        //             resultStorage.save(results);
-        //         },
-        //         deep: true
-        //     }
-        // },
     }
 });

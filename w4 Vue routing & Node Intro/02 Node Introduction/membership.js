ddabad2ac4115b63bf7784a83918ee635ac20109
anttus <anttu.suhonen@gmail.com>
2019-01-31 14:40:34 +0200

    w4_2 done

--- w4 Vue routing & Node Intro/02 Node Introduction/membership.js
+++ w4 Vue routing & Node Intro/02 Node Introduction/membership.js
@@ -1,33 +1,34 @@
-
-"use strict"
-
+'use strict'
 const readline = require('readline-sync')
 
-const type = String(readline.question('membership type (basic, premium, gold): ')).trim()
+const type = String(
+	readline.question('membership type (basic, premium, gold): ')
+).trim()
 const years = Number(readline.question('years required: '))
+let cost;
 
 try {
 	if (Number.isNaN(years) || years < 1) {
-	throw 'invalid number of years requested'
+		throw 'invalid number of years requested'
 	}
 	switch (type) {
-		case 'basic':
-			cost = 10.00
-			break
-		case 'premium':
-			cost = 15.00
-			break
-		case 'gold':
-			cost = 20.00
-			break
-		default:
-			throw 'invalid membership type'
+	case 'basic':
+		cost = 10.0
+		break
+	case 'premium':
+		cost = 15.0
+		break
+	case 'gold':
+		cost = 20.0
+		break
+	default:
+		throw 'invalid membership type'
 	}
 	cost *= years
 	if (years > 1) {
 		cost *= 0.8
 	}
-	console.log("membership cost is £"+cost)
-} catch(err) {
-console.log(err)
+	console.log('membership cost is £' + cost)
+} catch (err) {
+	console.log(err)
 }
